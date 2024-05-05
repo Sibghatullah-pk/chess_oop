92a4d08855686330a9ce48ee5bdfc2c11137f6c1
Hamza Ali <110694326+Hamza-blti@users.noreply.github.com>
2024-05-05 12:06:25 +0500

    Update chess_box.cpp

--- chess_box.cpp
+++ chess_box.cpp
@@ -1,76 +1,235 @@
 #include <iostream>
-
+#include <string>
+#include <iomanip>
 using namespace std;
 
-// piece encoding
-enum
+class Board
 {
-    e,
-    P,
-    N,
-    B,
-    R,
-    Q,
-    K,
-    p,
-    n,
-    b,
-    r,
-    q,
-    k,
-    o
-};
 
-// unicode pieces
-const char *unicode_pieces[] = {".", "♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚", " "};
+private:
+    int size = 8;
+    int label[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    
+protected:
+    string chessboard[8][8] = {
+        {"R", "H", "B", "Q", "K", "B", "H", "R"},
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {"r", "h", "b", "q", "k", "b", "h", "r"}};
 
-// chess board representation
-int board[128] = {
-    r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
-    p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
-    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
-    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
-    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
-    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
-    P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
-    R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o};
-
-// print board
-void print_board()
-{
-    // print new line
-    cout << endl;
-
-    // loop over board ranks
-    for (int rank = 0; rank < 8; rank++)
+public:
+    void displayBoard()
     {
-        for (int file = 0; file < 16; file++)
+        cout << " ";
+        for (int i = 0; i < size; ++i)
         {
-            // init square
-            int square = rank * 16 + file;
-
-            // print ranks
-            if (file == 0)
-                cout << " " << 8 - rank << "  ";
-
-            // if square is on board
-            if (!(square & 0x88))
-                cout << unicode_pieces[board[square]] << " ";
+            cout <<setw(3)<< label[i];
         }
-
-        // print new line every time new rank is encountered
         cout << endl;
+        for (int i = 0; i < size; ++i)
+        {
+            cout << label[i];
+            for (int j = 0; j < size; ++j)
+            {
+
+                cout << setw(3)<<chessboard[i][j] ;
+            }
+            cout << endl;
+        }
     }
 
-    // print files
-    cout << endl
-         << "    a b c d e f g h\n\n";
-}
+    void condition()
+    {
+    }
+};
 
-// main driver
+class knight : protected Board{
+
+public:
+   void validmove(int r , int c , int r_m , int c_m){
+          if( r - 1 == r_m && c + 2 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r + 1 == r_m && c + 2 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r - 2 == r_m && c + 1 == c_m){
+            chessboard[r_m][c_m] = "h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r - 2 == r_m && c - 1 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r - 1 == r_m && c - 2 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r + 1 == r_m && c - 2 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          } 
+          else if( r + 2 == r_m && c - 1 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+          else if( r + 2 == r_m && c + 1 == c_m){
+            chessboard[r_m][c_m] ="h";
+            chessboard[r][c]  = "__";
+          }
+   }
+      void board(){
+        displayBoard();
+      }
+};
+
+
+class Player : protected Board
+{
+private:
+    string userName, level;
+    int age , row, col, row_m, col_m;
+    knight h;
+public:
+    Player()
+    {
+        userName = "N/A";
+        level = "N/A";
+        age = 0;
+        
+        row = 0;
+        col = 0;
+
+        row_m = 0;
+        col_m = 0;
+    }
+
+
+    void set_entireDate()
+    {
+        cout << "\nEnter Your User Name:";
+        getline(cin,userName);
+        cout << "\nEnter Your Age:";
+        cin >> age;
+        cout << "\nEnter Your Level:";
+        cin >> level;
+        
+    }
+
+    void selection_piece()
+    {
+        
+        cout << "\nTo move your Desired Piece , Enter the Piece location according to the label:";
+        cout << "\nEnter row number:";
+        cin >> row; // 8
+        cout << "\nEnter coloumn number:";
+        cin >> col; // 7 
+
+        row = row - 1; // 7
+        col = col - 1;  // 6
+    }
+
+    void move_piece()
+    {
+
+        cout << "\nEnter the location to where you want to move the piece:";
+        cout << "\nEnter row number:";
+        cin >> row_m; // 6
+        cout << "\nEnter coloumn number:";
+        cin >> col_m; // 8
+         
+         row_m = row_m - 1 ; // 5 
+         col_m = col_m - 1 ;  //7
+           
+    }
+
+    void chk_peice()
+    {
+        if (chessboard[row ][col ] == "H"  || chessboard[row][col] == "h")
+        {
+         h.validmove(row , col ,row_m , col_m);
+         h.board();
+                
+        }
+        else if (chessboard[row][col] == "R")
+        {
+          
+        }
+        else if (chessboard[row][col] == "Q")
+        {
+            
+        }
+        else if (chessboard[row][col] == "B")
+        {
+           
+        }
+        else if (chessboard[row][col] == "P")
+        {
+        }
+        else if (chessboard[row][col] == "K")
+        {
+         
+        }
+        else
+        {
+          cout << "\nInvalid Move.";
+        }
+   
+    } 
+    void updated_Board(){
+        displayBoard();
+    }  
+};
+
+// class Pawn {
+// private:
+//     int currentRow;
+//     int currentCol;
+//     bool isWhite;
+
+// public:
+//     Pawn(int row, int col, bool white) : currentRow(row), currentCol(col), isWhite(white) {}
+
+//     bool isValidMove(int newRow, int newCol) const {
+//         // Check if the pawn is moving in the correct direction
+//         int direction = isWhite ? 1 : -1;
+//         if (newRow != currentRow + direction) {
+//            cout << "Invalid move: Pawns can only move one square forward.\n";
+//             return false;
+//         }
+
+//         // Check if the pawn is moving straight
+//         if (newCol == currentCol) {
+//             return true;
+//         }
+
+//         // Check if the pawn is capturing diagonally
+//         if (newCol == currentCol + 1 || newCol == currentCol - 1) {
+//             return true;
+//         }
+
+//         cout << "Invalid move: Pawns can only move forward or capture diagonally.\n";
+//         return false;
+//     }
+// };
+
+
 int main()
 {
-    print_board();
-    // ok
+    Board obj;
+    cout << "\nWelcome" << endl;
+    obj.displayBoard();
+
+    Player user1;
+      user1.selection_piece();
+      user1.move_piece();
+      user1.chk_peice();
+      
     return 0;
 }
